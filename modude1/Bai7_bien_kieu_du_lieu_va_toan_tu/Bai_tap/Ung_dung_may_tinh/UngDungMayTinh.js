029a15952c232a8de51586a7db2fafa5eaa4b11b
A0321I1-HuynhBaDan <huynhbadank16@gmail.com>
2021-04-11 21:22:05 +0700

    nopbaitapbai7

--- modude1/Bai7_bien_kieu_du_lieu_va_toan_tu/Bai_tap/Ung_dung_may_tinh/UngDungMayTinh.js
+++ modude1/Bai7_bien_kieu_du_lieu_va_toan_tu/Bai_tap/Ung_dung_may_tinh/UngDungMayTinh.js
@@ -1,11 +1,28 @@
-// function convert() {
-//     let a = document.getElementById('amount').value;
-//     document.getElementById('kq').innerHTML = a * 2;
-// };
 function cong() {
     let a = document.getElementById('number1').value;
     let b = document.getElementById('number2').value;
     let c = parseInt(a);
     let d = parseInt(b);
-    document.getElementById('kq').innerHTML = c + d ;
+    document.getElementById('kq').innerHTML = 'ResultAddition:' + c + d ;
+};
+function tru() {
+    let a = document.getElementById('number1').value;
+    let b = document.getElementById('number2').value;
+    let c = parseInt(a);
+    let d = parseInt(b);
+    document.getElementById('kq').innerHTML = 'Reult Subtraction:' + c - d ;
+};
+function nhan() {
+    let a = document.getElementById('number1').value;
+    let b = document.getElementById('number2').value;
+    let c = parseInt(a);
+    let d = parseInt(b);
+    document.getElementById('kq').innerHTML = 'Reult Multiplication:' + c * d ;
+};
+function chia() {
+    let a = document.getElementById('number1').value;
+    let b = document.getElementById('number2').value;
+    let c = parseInt(a);
+    let d = parseInt(b);
+    document.getElementById('kq').innerHTML = 'Reult Division:' + c / d ;
 };
