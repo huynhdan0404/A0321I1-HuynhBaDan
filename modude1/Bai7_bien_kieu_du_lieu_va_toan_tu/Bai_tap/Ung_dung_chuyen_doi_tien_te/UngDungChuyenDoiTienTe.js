05ef057ccbfbe9e04758081907d40f0dae53d99d
A0321I1-HuynhBaDan <huynhbadank16@gmail.com>
2021-04-12 23:16:40 +0700

    nopbaitapbai7

--- modude1/Bai7_bien_kieu_du_lieu_va_toan_tu/Bai_tap/Ung_dung_chuyen_doi_tien_te/UngDungChuyenDoiTienTe.js
+++ modude1/Bai7_bien_kieu_du_lieu_va_toan_tu/Bai_tap/Ung_dung_chuyen_doi_tien_te/UngDungChuyenDoiTienTe.js
@@ -1,4 +1,22 @@
+function currency() {
+    let a = document.getElementById('fcurrency').value;
+    let b = document.getElementById('tcurrency').value;
+    if (a == 'vn') {
+        document.getElementById('tcurrency').value = 'usd';
+    }else{
+        document.getElementById('tcurrency').value = 'vn';
+    };
+}
 function convert() {
-    let a = document.getElementById('amount').value;
-    document.getElementById('kq').innerHTML = a * 23000;
-};
+    let amount = document.getElementById('amount').value;
+    let fcurrency = document.getElementById('fcurrency').value;
+    let tcurrency = document.getElementById('tcurrency').value;
+    if (fcurrency == 'vn' && tcurrency == 'usd') {
+            document.getElementById('kq').innerHTML = 'Result: ' + amount /   23000 + ' USD';
+
+    };
+    if (fcurrency == 'usd' && tcurrency == 'vn') {
+            document.getElementById('kq').innerHTML= 'Result: ' + amount * 23000 + ' VND'
+    };
+}
+
